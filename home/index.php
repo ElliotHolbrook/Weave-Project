7231ad82dda669cdfeb642cf9d1a2608db61e101
Ells Bells <elliotbholbrook@gmail.com>
2023-03-17 19:35:21 +0000

    Update Home

--- home/index.php
+++ home/index.php
@@ -5,9 +5,16 @@
 		<link rel="stylesheet" href="../resources/dark-theme.css" id="theme" title="dark"/>
 	</head>
 	<body>
-		<input type="text">
-		You are fat
-		</input>
+		<tr>
+			<td style="border: 10px solid black">
+				<input type="text">
+				You are fat
+				</input>
+			</td>
+			<td style="border: 10px solid black">
+				Stinky Stinky
+			</td>
+		</tr>
 	</body>
 	<?php require_once '..\betawarning.php';?>
 </html>
