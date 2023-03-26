838d49c3bc74c6efc196823d37a645aec02cbe06
Ells Bells <elliotbholbrook@gmail.com>
2023-03-26 19:50:31 +0100

    26/03/2023 1
    Progress on home page layout

--- home/index.php
+++ home/index.php
@@ -5,16 +5,32 @@
 		<link rel="stylesheet" href="../resources/dark-theme.css" id="theme" title="dark"/>
 	</head>
 	<body>
-		<tr>
-			<td style="border: 10px solid black">
-				<input type="text">
-				You are fat
-				</input>
+		<table style="margin: 10px; padding: 20px; width: 100%; height: 100%">
+			<td style="height: 95vh; background-color: rgb(33,33,33); margin: 10px; padding: 10px; width: 15vw; border: 8px;">
+				<table style="width: 100%; height: 100%">
+					<tr style="width: 100%">
+					<td style="width: 100%; background-color: rgb(90,90,90); border-radius: 15px; position: relative; height: 250px">
+					<div style="position: absolute;  
+								top: 50%;  
+								bottom: 0;  
+								left: 50%;  
+								right: 0;  
+								margin: auto;
+								transform: translate(-50%, -50%);
+								"><img src="../resources/kayakraft-logo-HD.png"></img></div>
+					</td>
+					</tr>
+					<tr>
+					<td>
+					Hello
+					</td>
+					</tr>
+				</table>
 			</td>
-			<td style="border: 10px solid black">
+			<td>
 				Stinky Stinky
 			</td>
-		</tr>
+		</table>
 	</body>
 	<?php require_once '..\betawarning.php';?>
 </html>
