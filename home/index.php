27d4b721bee64670cb291dcc2f39af9435a5b460
Ells Bells <elliotbholbrook@gmail.com>
2023-03-29 22:58:00 +0100

    Switching to grid system for home

--- home/index.php
+++ home/index.php
@@ -4,26 +4,30 @@
 		<link rel="stylesheet" href="../resources/global-style.css"/>
 		<link rel="stylesheet" href="../resources/dark-theme.css" id="theme" title="dark"/>
 	</head>
-	<body>
+	<body style="display: grid">
 		<table style="margin: 10px; padding: 20px; width: 100%; height: 100%">
 			<td style="height: 95vh; background-color: rgb(33,33,33); margin: 10px; padding: 10px; width: 15vw; border: 8px;">
 				<table style="width: 100%; height: 100%">
 					<tr style="width: 100%">
-					<td style="width: 100%; background-color: rgb(90,90,90); border-radius: 15px; position: relative; height: 250px">
-					<div style="position: absolute;  
-								top: 50%;  
-								bottom: 0;  
-								left: 50%;  
-								right: 0;  
-								margin: auto;
-								transform: translate(-50%, -50%);
-								"><img src="../resources/kayakraft-logo-HD.png"></img></div>
+						<td style="width: 100%; background-color: rgb(90,90,90); border-radius: 15px; position: relative; height: 250px">
+							<div style="position: absolute;  
+										top: 50%;  
+										bottom: 0;  
+										left: 50%;  
+										right: 0;  
+										margin: auto;
+										transform: translate(-50%, -50%);
+										"><img src="../resources/kayakraft-logo-HD.png"></img></div>
+						</td>
+					</tr>
+					<tr style="padding-top: 10px;">
+					<td style="width: 100%; background-color: rgb(90,90,90); border-radius: 15px; position: relative; height: 30px;">
+						<div>
+							Hello
+						</div>
 					</td>
 					</tr>
 					<tr>
-					<td>
-					Hello
-					</td>
 					</tr>
 				</table>
 			</td>
@@ -31,6 +35,8 @@
 				Stinky Stinky
 			</td>
 		</table>
+		
+		<input type="text" style="background-color: white; border: 10px solid black; height: 30px">
 	</body>
 	<?php require_once '..\betawarning.php';?>
 </html>
