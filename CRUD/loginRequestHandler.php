662a902de03c06f1cd37bef9a90201c93b8ef028
Ells Bells <elliotbholbrook@gmail.com>
2023-04-26 22:51:03 +0100

    loginRequestHandler fix

--- CRUD/loginRequestHandler.php
+++ CRUD/loginRequestHandler.php
@@ -1,17 +1,23 @@
 <?php
+	//get database connection
 	$db = require "databaseConnection.php";
 	
+	//get data from the log in form
 	$email = $_GET["email"];
 	$password = $_GET["password"];
-		
-	echo $email . "<br>";
-	echo $password . "<br>";
 	
+	//prepare SQL statement
 	$sql = "SELECT passHashed FROM account_data WHERE email='{$email}'";
+	//prepare operation and point at db
 	$stmt = $db->prepare($sql);
+	//execute operation
+	$stmt->execute();
+	//fetch data from database
 	$passHashed = $stmt->fetch();
 	
-	if (password_verify($password, $passHashed)) {
+	
+	//verify user password against hashed database password
+	if (password_verify($password, $passHashed["passHashed"])) {
 		echo "Password Correct";
 	} else {
 		echo "Password Not Correct";
