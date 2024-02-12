7a1036097da27e8f1e933669519ea0339a4c20d7
Ells Bells <elliotbholbrook@gmail.com>
2024-02-12 14:26:14 +0000

    12/02/2024

--- Prototypes/Weave-Release/index.php
+++ Prototypes/Weave-Release/index.php
@@ -1,23 +1,25 @@
 <DOCTYPE html>
 <?php
     session_start();
-    //if (isset($_SESSION["account"])) {
-    //    header("Location: home");
-    //}
-
-    if(isset($_SESSION["tester"])) {
+    if (isset($_SESSION["account"])) {
+        header("Location: home");
+    } else {
         header("Location: login");
     }
+
+    // if(isset($_SESSION["tester"])) {
+    //     header("Location: login");
+    // }
 ?>
-<html>
+<!-- <html>
     <form style="position: absolute; left: 50%; top: 40%; translate: -50% -50%" method="post" action="index.php">
     <center><h3>KayaKraft Donator Testing</h3></center>  
     <center><input name="access-code" type="password" placeholder="Enter Access Code Here"></input></center>
     </form>
-</html>
+</html> -->
     <?php
-    if(isset($_POST["access-code"])) {
-        if($_POST["access-code"] == "dontateToKaya")
-        header("Location: login");
-        $_SESSION["tester"] = True;
-    }
+    // if(isset($_POST["access-code"])) {
+    //     if($_POST["access-code"] == "dontateToKaya")
+    //     header("Location: login");
+    //     $_SESSION["tester"] = True;
+    // }
